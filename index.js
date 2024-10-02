eb6c1c79a346b6965d7eafc2e7ffe4c4142cac03
Abhijit Aher <seabhi.aher@gmail.com>
2024-10-02 18:34:58 +0530

    added mongo connection and .env

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db_connection');  // Adjust the path if necessary
+require('dotenv').config(); 
+
 const app = express()
-const port = 3000
+connectDB()
+
+const port = process.env.PORT || 3000;
+console.log(port)
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
