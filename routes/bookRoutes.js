8135a38836542df5b6a1a0d77dc3c0ad8281f9e0
Abhijit Aher <seabhi.aher@gmail.com>
2024-10-06 17:19:10 +0530

    user schema timestamps

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const { createBook, getAllBooks, updateBook } = require("../controllers/bookController");
+const {
+  createBook,
+  getAllBooks,
+  updateBook,
+} = require("../controllers/bookController");
 
 const router = express.Router();
 
 router.post("/", createBook); // Create a book
 router.get("/", getAllBooks); // Get all books
-router.patch('/', updateBook); // Edit book
+router.patch("/", updateBook); // Edit book
 
 module.exports = router;
